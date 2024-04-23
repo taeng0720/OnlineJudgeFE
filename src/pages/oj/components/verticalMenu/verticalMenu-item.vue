8b54fa7b878a0f0275911b47ad78e3423dc36c7d
taeng <xodn0720@naver.com>
2024-04-23 14:43:50 +0900

    Update

---
+++ src/pages/oj/components/verticalMenu/verticalMenu-item.vue
@@ -0,0 +1,65 @@
+<template>
+  <li @click.stop="handleClick" :class="{disabled: disabled}">
+    <slot></slot>
+  </li>
+</template>
+
+<script>
+  import Emitter from '../mixins/emitter'
+
+  export default {
+    name: 'VerticalMenu-item',
+    mixins: [Emitter],
+    props: {
+      route: {
+        type: [String, Object]
+      },
+      disabled: {
+        type: Boolean,
+        default: false
+      }
+    },
+    methods: {
+      handleClick () {
+        if (this.route) {
+          this.dispatch('VerticalMenu', 'on-click', this.route)
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="less">
+  .disabled {
+    /*background-color: #ccc;*/
+    opacity: 1;
+    /*cursor: not-allowed;*/
+    pointer-events: none;
+    color: #ccc;
+    &:hover {
+      border-left: none;
+      color: #ccc;
+      background: #fff;
+    }
+  }
+
+  li {
+    border-bottom: 1px dashed #e9eaec;
+    color: #495060;
+    display: block;
+    text-align: left;
+    padding: 15px 20px;
+    &:hover {
+      background: #EFFFFB;
+      border-left: 2px solid #4F98CA;
+      color: #4F98CA;
+    }
+    & > .ivu-icon {
+      font-size: 16px;
+      margin-right: 8px;
+    }
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+</style>
